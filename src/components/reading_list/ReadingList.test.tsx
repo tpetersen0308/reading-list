18b5fd6512e32cc1243d68bd10c2ee334707a801
tpetersen0308 <travis.e.petersen@gmail.com>
2019-09-25 12:35:40 -0700

    Add feature to delete a reading list.

--- src/components/reading_list/ReadingList.test.tsx
+++ src/components/reading_list/ReadingList.test.tsx
@@ -110,6 +110,54 @@ describe("ReadingList", () => {
     expect(queryByText(/Book 2 Title/i)).toBeNull();
   });
 
+  it("can delete a reading list", async () => {
+    const data: IApiResponse["readingList"] = {
+      data: {
+        title: "Reading List",
+        readingListId: "reading-list-id",
+        books: [
+          {
+            title: "Book 1 Title",
+            authors: ["Book 1 Author 1", "Book 1 Author 2"],
+            image: "Book 1 Image",
+            dateCreated: "2019-09-20T17:29:28.566306",
+            bookId: "book 1 id",
+            readingListId: "reading-list-id"
+          },
+          {
+            title: "Book 2 Title",
+            authors: ["Book 2 Author 1", "Book 2 Author 2"],
+            image: "Book 2 Image",
+            dateCreated: "2019-08-20T17:29:28.566306",
+            bookId: "book 2 id",
+            readingListId: "reading-list-id"
+          }
+        ]
+      }
+    };
+
+    const match = {
+      isExact: false,
+      path: "/reading-list/:guid",
+      url: "/reading-list/reading-list-id",
+      params: {
+        guid: "reading-list-id"
+      }
+    };
+    const history: MemoryHistory = createMemoryHistory();
+    const location = createLocation(match.url);
+
+    const apiHandler: MockApiHandler = new MockApiHandler(data);
+
+    const { getByText } = render(<ReadingList match={match} location={location} history={history} user={user} setUser={jest.fn()} apiHandler={apiHandler} />);
+
+    await wait(() => getByText(/Reading List:/i));
+
+    fireEvent.click(getByText(/Delete List/i));
+
+    expect(history.location.pathname).toEqual("/");
+  });
+
   it("renders error messages", async () => {
     const data: IApiResponse["readingList"] = {
       error: {
